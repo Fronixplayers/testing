7b92021809f776438e0e7482ccca73a68ce75493
Fronixplayers <fronixplayers@gmail.com>
2024-01-21 20:59:09 +0530

    secondcommit

--- bot/response.js
+++ bot/response.js
@@ -1,6 +1,18 @@
 const responseObj = {
-  hello: "Hey ! How are you doing ?",
-  hey: "Hey! What's Up",
+  Hello: "Hey ! How are you doing ?",
+  
+  Hello: "Hey ! How are you doing ?",
+  
+  Hey: "Hey! What's Up",
+  
+  Needcapcut:"coming soon",
+  
+  Needbio:"coming soon",
+  
+  Needbegginertraining:"coming soon",
+
+  Whatisfronix:"coming soon",
+  
   today: new Date().toDateString(),
   time: new Date().toLocaleTimeString(),
 };
