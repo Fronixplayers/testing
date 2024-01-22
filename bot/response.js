d37f2fc5d646bad777709d49c306c561be4aa505
Fronixplayers <fronixplayers@gmail.com>
2024-01-22 19:28:32 +0530

    secondcommit

--- bot/response.js
+++ bot/response.js
@@ -1,6 +1,18 @@
  const responseObj = {
-  Hi: "Hey how can I help you?",
-  hey: "Hey! What's Up",
-  today: new Date().toDateString(),
-  time: new Date().toLocaleTimeString(),
+  Hi: "Hey 1.Menu, 2.Capcut, 3.Training.",
+  
+  Hey: "Hey 1.Menu, 2.Capcut, 3.Training.",
+  
+  Code:"361369", 
+
+  Menu:"1.Menu,2.Google link, 3.proof.",
+
+  Hello:"Hey 1.Menu, 2.Capcut, 3.Training.",
+
+  Training:"https://youtu.be/ptiG2NsnjNc?feature=shared copy Please",
+
+  Today: new Date().toDateString(),
+  
+  Time: new Date().toLocaleTimeString(),
 };
+ 
