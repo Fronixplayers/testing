a39ca2b0a8890752a1c80b01fca01b47d585ccfe
Fronixplayers <fronixplayers@gmail.com>
2024-01-24 20:27:05 +0530

    secondcommit

--- static/scripts/responses.js
+++ static/scripts/responses.js
@@ -1,19 +1,39 @@
 function getBotResponse(input) {
     //rock paper scissors
-    if (input == "rock") {
+    if (input == "Rock") {
         return "paper";
-    } else if (input == "paper") {
+    } else if (input == "Paper") {
         return "scissors";
-    } else if (input == "scissors") {
+    } else if (input == "Scissors") {
         return "rock";
     }
 
     // Simple responses
-    if (input == "hello") {
-        return "Hello there!";
-    } else if (input == "goodbye") {
+    if (input == "Menu") {
+        return "1.capcut, 2.training, 3.next!";
+    } else if (input == "Capcut") {
+        return "check in tools";
+    } else {
+        return "Kuch or Puchle Iske alawa?";
+    }
+
+    //working
+
+    if (input == "Hello") {
+        return "Hello FronixBot Here, how can i help you?";
+    } else if (input == "Bye") {
         return "Talk to you later!";
     } else {
-        return "Try asking something else!";
+        return "Kuch or Puchle Iske alawa?";
+    }
+
+    //Hal Chal
+
+    if (input == "How are you?") {
+        return "I'am Fine how can i help you?";
+    } else if (input == "Bye") {
+        return "Talk to you later!";
+    } else {
+        return "Kuch or Puchle Iske alawa?";
     }
 }
