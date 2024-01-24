bad35c8197e1983e81aaa0a5e17b5856d1b27b3e
Fronixplayers <fronixplayers@gmail.com>
2024-01-24 20:33:10 +0530

    secondcommit

--- static/scripts/chat.js
+++ static/scripts/chat.js
@@ -34,7 +34,7 @@ function getTime() {
 
 // Gets the first message
 function firstBotMessage() {
-    let firstMessage = "How's it going?"
+    let firstMessage = "Yes FronixBot is Here For your help?"
     document.getElementById("botStarterMessage").innerHTML = '<p class="botText"><span>' + firstMessage + '</span></p>';
 
     let time = getTime();
