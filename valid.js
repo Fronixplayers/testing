02565d1704ac7f260cec69151246497e5d59e54f
Fronixplayers <fronixplayers@gmail.com>
2024-01-26 22:22:04 +0530

    secondcommit

--- valid.js
+++ valid.js
@@ -2,7 +2,7 @@
 
 	function checkPassword() {
 		var password = document.getElementById("password").value;
-		if (password === "fronix@") {
+		if (password === "growwmore") {
 		  window.location.href = "video.html";
 		} else {
 		  document.getElementById("message").innerHTML = "Incorrect code. Please take code or pay for code.";
