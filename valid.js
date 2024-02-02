b95938dd47cce97706dc6b6a83323769c739b66d
Fronixplayers <150437943+Fronixplayers@users.noreply.github.com>
2024-02-03 04:54:59 +0530

    Update valid.js

--- valid.js
+++ valid.js
@@ -2,7 +2,7 @@
 
 	function checkPassword() {
 		var password = document.getElementById("password").value;
-		if (password === "telgisp") {
+		if (password === "uk07") {
 		  window.location.href = "video.html";
 		} else {
 		  document.getElementById("message").innerHTML = "Incorrect code. Please take code or pay for code.";
